14c34d5c65c1c7d513f715726b3a8ec38787da34
zombiej <smith3816@gmail.com>
2020-05-13 15:18:44 +0800

    unique cloneElement

--- components/_util/reactNode.ts
+++ components/_util/reactNode.ts
@@ -1,8 +1,17 @@
 import * as React from 'react';
 
-// eslint-disable-next-line import/prefer-default-export
-export function cloneElement(element: React.ReactNode, ...restArgs: any[]) {
-  if (!React.isValidElement(element)) return element;
+export function replaceElement(
+  element: React.ReactNode,
+  invalidateReplacement: React.ReactNode,
+  ...restArgs: any[]
+) {
+  if (!React.isValidElement(element)) {
+    return invalidateReplacement;
+  }
 
   return React.cloneElement(element, ...restArgs);
+}
+
+export function cloneElement(element: React.ReactNode, ...restArgs: any[]) {
+  return replaceElement(element, element, ...restArgs);
 }
